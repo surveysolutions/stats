3335780bc7a9965e9e9e71ff228239d0bf79a40f
build server <tc@build.mysurvey.solutions>
2021-12-16 12:30:14 -0500

    new chart on client installations

--- script.js
+++ script.js
@@ -122,3 +122,34 @@ var myChart = new Chart(ctx, {
         },
     }
 });
+
+var ctx = document.getElementById('localServers');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'unique ip addresses seen within the month',
+            data: localServers,
+            backgroundColor: 'rgba(75, 160, 100, 0.5)',
+            borderColor: 'rgb(75, 160, 100)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Running client servers'
+            }
+        },
+        scales: {
+            xAxes: {
+              type: 'time',
+              time: {
+                tooltipFormat: 'MMM yyyy',
+                parser: 'yyyy-MM-dd',
+              }
+            }
+        },
+    }
+});
